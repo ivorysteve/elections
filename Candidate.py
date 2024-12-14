d625feecda2bb5d5e516bc31749e1291016b8a9c
Stephen Gilbane <sgilbane@gmail.com>
2024-12-14 16:22:42 -0500

    Create Candidate.py

---
+++ Candidate.py
@@ -0,0 +1,14 @@
+#
+# A candidate who received votes.
+#
+class Candidate:
+    def __init__(self, office):
+        self.office = office
+        self.name = 'UNKNOWN'
+        self.votes_ed = 0
+        self.votes_mail = 0
+        self.votes_prov = 0
+
+    # String method
+    def __str__(self):
+        return f"{self.name}: {self.votes_ed} election day, {self.votes_mail} mail-in, {self.votes_prov} provisional"
