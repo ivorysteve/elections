459ffbe0bbb21d3c1be50dd3b3c945e99ca773d8
Stephen Gilbane <sgilbane@gmail.com>
2024-12-14 16:23:19 -0500

    Create TemplateRecord.py

---
+++ TemplateRecord.py
@@ -0,0 +1,24 @@
+# A single record of the output
+#
+# Reference: https://docs.google.com/spreadsheets/d/13IMTlAxVFoon8_DfyJBP58-r2n9kmHLlKdBbI2kDQVQ/edit?gid=272640954#gid=272640954
+#
+class TemplateRecord:
+    def __init__(self):
+        election = ''
+        state = '' 
+        county  = ''
+        precinct = ''
+        jurisdiction = ''
+        office = ''
+        candidate = ''
+        party = ''
+        vote_mode = ''
+        votes = ''
+        writein = ''
+        result_status = ''
+        source_url = ''
+        source_filename = ''
+
+    # String method
+    def __str__(self):
+        return f"{self.election},{self.state},{self.county},{self.precinct},{self.jurisdiction},{self.office},{self.candidate},{self.party},{self.vote_mode},{self.votes},{self.writein},{self.result_status}"
