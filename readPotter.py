5083c53448fee9881126a683ce65f3aa46aea390
Stephen Gilbane <sgilbane@gmail.com>
2024-12-23 20:41:17 -0500

    Finish Potter.

--- readPotter.py
+++ readPotter.py
@@ -3,7 +3,7 @@ from ElectionGlobals import Globals
 from FormatSpec import FormatSpec
 
 fmtSpec = FormatSpec('Potter')
-fmtSpec.header_field_count = 4
+fmtSpec.header_field_count = 3
 fmtSpec.datetime_value = '11/25/2024 08:38 AM'
 
 readCountyResults(Globals.STATE, Globals.STATE_ABBREV, fmtSpec)
