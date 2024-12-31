f5b0e545ef4a442c1914a3c369b76424f77a4c23
Stephen Gilbane <sgilbane@gmail.com>
2024-12-30 19:13:50 -0500

    Finish Mifflin.

--- bin/readMifflin.py
+++ bin/readMifflin.py
@@ -3,7 +3,10 @@ from ElectionGlobals import Globals
 from FormatSpec import FormatSpec
 
 fmtSpec = FormatSpec('Mifflin')
-fmtSpec.header_field_count = 5
+fmtSpec.header_field_count = 1
+fmtSpec.precinct_name_index = 0
+fmtSpec.datetime_value = '11/25/2024 09:39AM'
+fmtSpec.results_type_value = 'OFFICIAL RESULTS'
 
 readCountyResults(Globals.STATE, Globals.STATE_ABBREV, fmtSpec)
 
